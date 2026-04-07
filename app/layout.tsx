7551eaadaa2da4edd4ac86dd08178f2b333281e8
bonny503 <mwangikariuki503@gmail.com>
2026-04-07 17:36:06 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 export const metadata = {
-  title: 'SecureLink',
-  description: 'Internet and CCTV',
-}
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
-}
+  title: 'SecureLink Kenya | CCTV & Apartment WiFi',
+  description: 'Expert CCTV and WiFi in Nairobi, Kiambu, and Mombasa. We accept Pi Network, USDT, and RUSD.',
+  verification: {
+    google: '5z2lDnQ6mdG9S2qZm74DNfOk3xdwLR-orzDHc5XiJxs',
+  },
+};
